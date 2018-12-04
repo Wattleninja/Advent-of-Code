69c7105414c5a85abebc6080e0ea57fe180cfc35
Steven Summers <steven.summers@uq.net.au>
2018-12-04 19:33:21 +1000

    Day 3 Solution

---
+++ 2018/aoc_03.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+
+def main():
+    with open('03.in') as f:
+        overlaps = defaultdict(int)
+        claims = defaultdict(list)
+
+        for line in f:
+            id_, _, offsets, dim = line.strip()[1:].split()
+            x, y = map(int, offsets[:-1].split(','))
+            w, h = map(int, dim.split('x'))
+
+            for i in range(x, x + w):
+                for j in range(y, y + h):
+                    v = (i, j)
+                    claims[id_].append(v)
+                    overlaps[v] += 1
+
+    print(sum(1 for count in overlaps.values() if count >= 2))
+    for claim_id, claim_list in claims.items():
+        if all(overlaps[claim] == 1 for claim in claim_list):
+            print(claim_id)
+            break
+
+
+if __name__ == '__main__':
+    main()
