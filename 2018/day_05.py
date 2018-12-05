d532168e51daa8e696637a0923e9104cc693b207
Steven Summers <steven.summers@uq.net.au>
2018-12-05 17:07:03 +1000

    Dsy 5 Solution

---
+++ 2018/day_05.py
@@ -0,0 +1,29 @@
+from string import ascii_lowercase
+
+
+def react(s):
+    stack = []
+    for char in s:
+        if stack and stack[-1] == char.swapcase():
+            stack.pop()
+        else:
+            stack.append(char)
+    return ''.join(stack)
+
+
+def main():
+    with open('05.in') as f:
+        data = f.read().strip()
+
+    print(len(react(data)))
+
+    reacts = []
+    for char in ascii_lowercase:
+        s = react(data.translate(str.maketrans('', '', char + char.upper())))
+        reacts.append(len(s))
+
+    print(min(react))
+
+
+if __name__ == '__main__':
+    main()
