c1f702c1458b43cac50a865ba7ffaaefab6a7078
Steven Summers <steven.summers@uq.net.au>
2018-12-12 23:51:45 +1000

    Fix diff check for early break

--- 2018/day_12.py
+++ 2018/day_12.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def main():
     mapping = {}
     with open('inputs/12.in') as f:
@@ -14,7 +17,7 @@ def main():
     prev_gen = initial
     pot_zero = initial.find('#')
     prev = 0
-    prev_diff = None
+    diffs = deque(maxlen=5)
     for i in range(1, 50_000_000_000 + 1):
         for pot in range(2, len(prev_gen) - 2):
             group = prev_gen[pot - 2:pot + 3]
@@ -25,14 +28,14 @@ def main():
         prev_gen = ''.join(generation)
         # print(f'{i:>3}: {prev_gen}')
         pot_sum = sum(i for i, c in enumerate(prev_gen, start=-pot_zero) if c == '#')
-
-        if prev_diff == pot_sum - prev:
+        diff = pot_sum - prev
+        diffs.append(diff)
+        if len(diffs) > 1 and all(x == diff for x in diffs):
             break
-        prev_diff = pot_sum - prev
         prev = pot_sum
 
     print(pot_sum)
-    print((50_000_000_000 - i) * prev_diff + pot_sum)
+    print((50_000_000_000 - i) * diff + pot_sum)
 
 
 if __name__ == '__main__':
